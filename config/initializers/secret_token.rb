b658c9b1a21ce21bcfdfc9ff67872bc640094873
Jerald Jackson <jerald.jackson@gmail.com>
2014-02-01 18:49:00 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinga::Application.config.secret_key_base = 'bbb9541febcd34eafd35f09aeb95245004f4e68b21e8c882a7141af3f82af97f1cf0f6fb6bb890a5f7d2733197f58b48dab47ffdbcfbf3f30fc72cedac0cc189'
